1c99bd6bf8623f6e6fcfbaf8ec392e6e029a41b0
kapil jhajhria <jhajhria44@gmail.com>
2019-10-18 18:00:27 +0530

    Almost done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'news_api_helper.dart';
 
 void main() => runApp(MaterialApp(
       home: NewsListPage(),
@@ -10,6 +11,25 @@ class NewsListPage extends StatefulWidget {
 }
 
 class _NewsListPageState extends State<NewsListPage> {
+  NetworkHelper getNewsJsonLink = NetworkHelper(countryName: 'in');
+  Map newsMap;
+  bool fetchedNews = false;
+
+  @override
+  void initState() {
+    fetchingNewsData();
+    super.initState();
+  }
+
+  void fetchingNewsData() async {
+    fetchedNews = false;
+    print('will try to fetch news');
+    newsMap = await getNewsJsonLink.fetchNewsMapFromURL();
+    print('news fetched and itemCOunt should be ${newsMap['articles'].length}');
+    fetchedNews = true;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +41,119 @@ class _NewsListPageState extends State<NewsListPage> {
               fontSize: 32, color: Colors.black, fontFamily: 'OldLondon'),
         ),
       ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: FlatButton(
+                child: Text('getNewsAPI'),
+                onPressed: () {
+                  setState(() {});
+                },
+              ),
+            ),
+            Expanded(
+              flex: 8,
+              child: fetchedNews
+                  ? HomeScreenNewsCardList(
+                      fetchedNews: fetchedNews, newsMap: newsMap)
+                  : Center(
+                      child: CircularProgressIndicator(
+                        strokeWidth: 3.0,
+                        backgroundColor: Colors.red,
+                      ),
+                    ),
+            )
+          ],
+        ),
+      ),
     );
+  }
+}
+
+class HomeScreenNewsCardList extends StatelessWidget {
+  const HomeScreenNewsCardList({
+    Key key,
+    @required this.fetchedNews,
+    @required this.newsMap,
+  }) : super(key: key);
+
+  final bool fetchedNews;
+  final Map newsMap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: newsMap['articles'].length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Column(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.topLeft,
+                  child: Text(newsMap['articles'][index]['title'],
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        width: 250,
+                        margin: EdgeInsets.all(2.0),
+                        child: Text(
+                          newsMap['articles'][index]['description'],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        width: 150,
+                        margin: EdgeInsets.all(12.0),
+                        child: Image.network(
+                            newsMap['articles'][index]['urlToImage']),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 4,
+                      child: Container(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                            newsMap['articles'][index]['source']['name'],
+                            style: TextStyle(color: Colors.grey)),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        margin: EdgeInsets.all(2.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Icon(Icons.share),
+                            Icon(Icons.bookmark_border)
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  color: Colors.black,
+                  height: 2.0,
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
---
+++ lib/news_api_helper.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+
+class NetworkHelper {
+  String url;
+  String country;
+  String myNewsApiKey = "YOUR API KEY HERE";
+
+  NetworkHelper({String generateUrl, String countryName}) {
+    this.country = countryName;
+    this.url =
+        'https://newsapi.org/v2/top-headlines?country=$countryName&apiKey=$myNewsApiKey';
+  }
+
+  Future<Map> fetchNewsMapFromURL() async {
+    print('reached here inside fewtch');
+    print('url here is $url');
+    Response response = await get(url);
+    if (response.statusCode == 200) {
+      print("response 200");
+    }
+    Map<String, dynamic> newsDataMap = jsonDecode(response.body);
+    print(newsDataMap['articles'][0]['title']);
+    return newsDataMap;
+  }
+}
+
+//top india headlines
+//https://newsapi.org/v2/top-headlines?country=in&apiKey=$myNewsApiKey
