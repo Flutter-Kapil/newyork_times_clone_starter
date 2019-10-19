4670dfd02d8b3dba4a96035f291d2021e22f9cd7
kapil jhajhria <jhajhria44@gmail.com>
2019-10-19 12:48:09 +0530

    changed fonts

--- lib/news_api_helper.dart
+++ lib/news_api_helper.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart';
 class NetworkHelper {
   String url;
   String country;
-  String myNewsApiKey = "";
+  String myNewsApiKey = "c";
 
   NetworkHelper({String generateUrl, String countryName}) {
     this.country = countryName;
