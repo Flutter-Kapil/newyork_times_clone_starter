031eb520fe323786fbb78e934e32865d75fae1a4
kapil jhajhria <jhajhria44@gmail.com>
2019-11-09 14:15:48 +0530

    Revert "custom hero animation"
    This reverts commit 2b243f4b

--- lib/main.dart
+++ lib/main.dart
@@ -206,9 +206,6 @@ class HomeScreenNewsCardList extends StatelessWidget {
                           child: Container(
                             margin: EdgeInsets.all(2.0),
                             child: Hero(
-                              createRectTween: (begin, end) {
-                                return CustomRectTween(a: begin, b: end);
-                              },
                               tag: newsMap['articles'][index]['title'],
                               child: newsMap['articles'][index]['urlToImage'] !=
                                       null
@@ -271,40 +268,5 @@ class HomeScreenNewsCardList extends StatelessWidget {
             ),
           );
         });
-  }
-}
-
-double lerpDouble(num a, num b, double t) {
-  if (a == null && b == null) return null;
-  a ??= 0.0;
-  b ??= 0.0;
-  return a + (b - a) * t;
-}
-
-class CustomRectTween extends RectTween {
-  CustomRectTween({this.a, this.b}) : super(begin: a, end: b);
-  final Rect a;
-  final Rect b;
-
-  @override
-  Rect lerp(double t) {
-    Curves.elasticOut.transform(t);
-    //any curve can be applied here e.g. Curve.elasticOut.transform(t);
-    final verticalDist = Cubic(0.72, 0.15, 0.5, 1.23).transform(t);
-
-    final top = lerpDouble(a.top, b.top, t) * (1 - verticalDist);
-    return Rect.fromLTRB(
-      lerpDouble(a.left, b.left, t),
-      top,
-      lerpDouble(a.right, b.right, t),
-      lerpDouble(a.bottom, b.bottom, t),
-    );
-  }
-
-  double lerpDouble(num a, num b, double t) {
-    if (a == null && b == null) return null;
-    a ??= 0.0;
-    b ??= 0.0;
-    return a + (b - a) * t;
   }
 }
